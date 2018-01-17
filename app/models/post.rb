5040c548760d92f0fa423d5480550860fd414377
mxdavis <md01@me.com>
2018-01-17 14:17:53 +0200

    add method to generate a factorial number, make sure it was not the same factorial as previously on update, and create callback on save and update to call this method

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,13 @@
 class Post < ApplicationRecord
+  after_save :generate_factorial
+
+  def generate_factorial
+    i = rand(1..10)
+
+    while (self.factorial == (1..i).reduce(:*))
+      i = rand(1..10)
+    end
+
+    self.factorial = (1..i).reduce(:*)
+  end
 end
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -16,5 +16,28 @@ RSpec.describe Post, type: :model do
       post = build(:post)
       expect(post.factorial).to equal(0)
     end
+
+    it 'creates a new factorial on save' do
+      post = build(:post)
+      post.save
+      expect([1, 2, 6, 24, 120, 720, 5040, 40320, 362880, 3628800]).to include(post.factorial)
+    end
+
+    it 'creates a new factorial on update' do
+      post = build(:post)
+      post.save
+      save_factorial = post.factorial
+      post.update(title: "this is an updated title")
+      updated_factorial = post.factorial
+      expect([1, 2, 6, 24, 120, 720, 5040, 40320, 362880, 3628800]).to include(post.factorial)
+      expect(save_factorial).not_to eq(updated_factorial)
+    end
+  end
+
+  describe 'methods' do
+    it 'generate_factorial generates a factorial number from a sample between 1 and 10' do
+      post = build(:post)
+      expect([1, 2, 6, 24, 120, 720, 5040, 40320, 362880, 3628800]).to include(post.generate_factorial)
+    end
   end
 end
