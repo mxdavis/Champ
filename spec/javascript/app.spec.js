5d5cdac612eda093120c37b71978fd27988d7469
mxdavis <md01@me.com>
2018-01-17 18:01:47 +0200

    make sure the app is rendering with testing

---
+++ spec/javascript/app.spec.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {shallow} from 'enzyme';
+import App from '../../app/javascript/packs/components/app.jsx'
+
+
+it('renders without crashing', () => {
+    shallow(<App />);
+});
+
+it('renders an h1', () => {
+  const app = shallow(<App />);
+
+  expect(app.text()).toBe("Some of our Great Posts");
+});
