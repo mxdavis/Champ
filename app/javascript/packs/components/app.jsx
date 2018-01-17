b8ef5b711be013f5b7264e625a687e0771a61a1d
mxdavis <md01@me.com>
2018-01-17 18:58:49 +0200

    fetch posts and add to state, pass into the Posts component

--- app/javascript/packs/components/app.jsx
+++ app/javascript/packs/components/app.jsx
@@ -1,10 +1,33 @@
 import React, { Component } from 'react';
 
+import apiRequests from '../modules/apiRequests.js'
+
+
 class App extends Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      posts: [],
+      error: ""
+    }
+  }
+
+  componentDidMount() {
+    this.fetchPosts()
+  }
+
+  fetchPosts() {
+    apiRequests.get('posts')
+      .then(posts => this.setState({posts}))
+      .catch(error => this.setState({error}))
+  }
+
   render() {
     return (
       <div className="App">
         <h1>Some of our Great Posts</h1>
+        <Posts posts={this.state.posts}/>
       </div>
     );
   }
